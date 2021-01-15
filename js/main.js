8e26683a460302c2779adae942d3eb8e59d5d4b7
Jalia Evans <jaliaevans@Jalias-MBP.fios-router.home>
2021-01-15 15:23:34 -0500

    deploy update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let option;
 
-if ($('#bmw-sample')) {
+// bmw
+if ($('#bmw-sample').length) {
     var bmwSample = document.getElementById('bmw-sample');
     bmwSample.style.height = '380px';
     bmwSample.style.width = '100%';
@@ -28,7 +29,11 @@ if ($('#bmw-sample')) {
         option = {
             legend: {
                 data: ['Awardees', 'Applicants'],
-                top: 0
+                top: 0,
+                textStyle: {
+                    fontFamily: 'menlo, sans-serif',
+                    fontSize: 12
+                }
             },
             xAxis:{
                 axisTick: {show: false},
@@ -36,7 +41,9 @@ if ($('#bmw-sample')) {
                 name: 'Fiscal Years',
                 nameLocation: 'middle',
                 nameTextStyle: {
-                    padding: [15, 0, 0, 0]
+                    padding: [15, 0, 0, 0],
+                    fontFamily: 'menlo, sans-serif',
+                    fontSize: 12
                 }
             },
             grid: {
@@ -59,7 +66,9 @@ if ($('#bmw-sample')) {
                     },
                     nameLocation: 'middle',
                     nameTextStyle: {
-                        padding: [0, 0, 25, 0]
+                        padding: [0, 0, 25, 0],
+                        fontFamily: 'menlo, sans-serif',
+                        fontSize: 12
                     }
                 }
             ],
@@ -152,22 +161,150 @@ if ($('#bmw-sample')) {
         // table build
         if ($('#bmwTable')) {
             let bmwTable = $('#bmwTable');
+            let bmwBody = $('#bmwTable tbody');
             let tr;
             let rows = [];
+
+            bmwTable.prepend(`<thead><th>Fiscal Year</th><th>Applicants, n</th><th>Awardees, n</th></thead>`);
     
             data.forEach((e) => {
                 tr = `<tr><td data-label="Fiscal Year">${e.fiscalYear}</td><td data-label="Applicants">${e.applicants}</td><td data-label="Awardees">${e.awardees}</td></tr>`;
                 rows.push(tr);
             });
-
-            bmwTable.append(`<tr><thead><th>Fiscal Year</th><th>Applicants, n</th><th>Awardees, n</th></thead></tr>`);
-            bmwTable.append(rows);
+            bmwBody.append(rows);
         }
     });
 
-    window.onresize = () => bmwInstance.resize();
-}
+    var disconnectLineGraph = document.getElementById('disconnectedLineGraph');
+    var heightLineGraph = '380px';
+    var widthLineGraph = '32%'
+    var displayLineGraph = 'inline-block';
 
+    if (window.innerWidth < 768) {
+        widthLineGraph = '100%'
+    }
+
+    disconnectLineGraph.style.height = heightLineGraph;
+    disconnectLineGraph.style.width = widthLineGraph;
+    disconnectLineGraph.style.display = displayLineGraph;
+
+    var disconnectLineInstance = echarts.init(disconnectLineGraph);
+
+    option = {
+        legend: {
+            data: ['Black, Green, Red, Orange']
+        },
+        title: {
+            text: 'With Gaps',
+            textAlign: 'center',
+            left: '120',
+            textStyle: {
+                fontFamily: 'menlo, sans-serif',
+                fontSize: 16
+            }
+        },
+        grid: {
+            left: 50
+        },
+        xAxis: {type: 'category'},
+        yAxis: {},
+        series: [
+            {
+                name: 'Black',
+                type: 'line',
+                color: '#000',
+                lineStyle: {
+                    width: 3
+                },
+                data: [0, 120, 50, undefined, 100, 90, 230, 210]
+            }
+        ]
+    }
+
+    disconnectLineInstance.setOption(option);
+
+
+    var autoConnectGraph = document.getElementById('autoConnectedLineGraph');
+    autoConnectGraph.style.height = heightLineGraph;
+    autoConnectGraph.style.width = widthLineGraph;
+    autoConnectGraph.style.display = displayLineGraph;
+
+    var autoConnectInstance = echarts.init(autoConnectGraph);
+
+    option.series = [
+        {
+            name: 'Black',
+            type: 'line',
+            connectNulls: true,
+            color: '#000',
+            lineStyle: {
+                width: 3
+            },
+            data: [0, 120, 50, undefined, 100, 90, 230, 210]
+        }
+    ]
+
+    option.title.text = "Connected Nulls"
+
+    autoConnectInstance.setOption(option);
+
+    var solutionLineGraph = document.getElementById('solutionLineGraph');
+    solutionLineGraph.style.height = heightLineGraph;
+    solutionLineGraph.style.width = widthLineGraph;
+    solutionLineGraph.style.display = displayLineGraph;
+
+    solutionLineInstance = echarts.init(solutionLineGraph);
+
+    option.series = [
+        {
+            name: 'Black',
+            type: 'line',
+            color: '#aaa',
+            connectNulls: true,
+            lineStyle: {
+                width: 2,
+                type: 'dashed'
+            },
+            data: [0, 120, 50, undefined, 100, 90, 230, 210]
+        },
+        {
+            name: 'Black',
+            type: 'line',
+            color: '#000',
+            lineStyle: {
+                width: 3
+            },
+            data: [0, 120, 50, undefined, 100, 90, 230, 210]
+        }
+    ]
+
+    option.title.text = "My Solution"
+
+    solutionLineInstance.setOption(option);
+
+    window.onresize = () => {
+        // bmwSample.style.width = '100%';
+        if (window.innerWidth < 768) {
+            widthLineGraph = '100%'
+            heightLineGraph = '250px';
+        } else {
+            widthLineGraph = '32%';
+        }
+
+        disconnectLineGraph.style.height = heightLineGraph;
+        autoConnectGraph.style.height = heightLineGraph;
+        solutionLineGraph.style.height = heightLineGraph;
+
+        disconnectLineGraph.style.width = widthLineGraph;
+        autoConnectGraph.style.width = widthLineGraph;
+        solutionLineGraph.style.width = widthLineGraph;
+
+        disconnectLineInstance.resize();
+        autoConnectInstance.resize();
+        solutionLineInstance.resize();
+        bmwInstance.resize();
+    }
+}
 
 if ($('#bmwFlipper').on('click', (e) => {
     if (e.currentTarget.parentNode.childNodes[1].childNodes[1].classList.value.includes('flipped')) {
@@ -177,3 +314,10 @@ if ($('#bmwFlipper').on('click', (e) => {
     }
     
 }));
+
+// scroll to top
+if ($('#backToTop')) {
+    $('#backToTop').on('click', () => {
+        $("html, body").animate({scrollTop: 0}, 600);
+    });
+}
